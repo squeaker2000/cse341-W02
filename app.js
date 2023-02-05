9c715ff7942646e6701a5e425c04662e532c4ebd
squaker2000 <elizabeth@orchards.ws>
2023-02-04 17:09:35 -0700

    test runs

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ app
   })
   .use('/', require('./routes'));
 
-// mongodb.initDb((err, mongodb) => {
-//   if (err) {
-//     console.log(err);
-//   } else {
-//     app.listen(port);
-//     console.log(`Connected to DB and listening on ${port}`);
-//   }
-// });
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
